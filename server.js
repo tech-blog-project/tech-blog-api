aba143acf008dbbe9f4811d14da7d903a3cf2cfb
Moutwei <moutweileechap@gmail.com>
2019-11-05 10:58:15 -0500

    Configured server
    - required new route files in server.js
        (comment and entry routes)
    - then registered them

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const entryRoutes = require('./app/routes/entry_routes')
+const commentRoutes = require('./app/routes/comment_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -65,6 +67,8 @@ app.use(requestLogger)
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(entryRoutes)
+app.use(commentRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
